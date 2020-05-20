c2f38903524e150c3dcb3fa751740e0d7e7f53b8
mor cohen <morcohen1515@gmail.com>
2020-05-20 22:00:20 +0300

    appdate model file

--- src/model/main.java
+++ src/model/main.java
@@ -3,7 +3,35 @@ package model;
 public class main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//יצירת סקאוט
+		scout s=new scout();
+		scout s1=new scout("mor","hapoel tel aviv","mC1527101215",204272231,null);
+		scout s2=new scout("oz","hapoel tel aviv","mC1527101215",204272231,null);
+		s.addScout(s1);
+		s.addScout(s2);
+		
+		//יצירת שחקן חדש
+		player p = new player();
+		player p1 = new player();
+		
+	    //הכנסה לרשימה
+	    player_List players = new player_List();
+	    players.addplayer(p);
+	    players.addplayer(p1);
+
+	    //יצירת משחק לשחקן
+	    game g = new game("15.5.20",1,0,94,"Beitar Jerusalem",1,3);
+	    game g1 = new game("17.5.20",1,0,60,"maccabi hanazim",0,2);
+	    game g2 = new game("21.5.20",1,0,45,"maccabi haifa",1,0);
+	    
+	    //הכנסה של משחק לרשימה שחקן
+	   p.addGame(g);
+
+		//רשימת סקאוטרים
+		System.out.println(p.getSetOfGames());
+		
+		//רשימת שחקנים
+		System.out.println(players.getListPlayer());		
 
 	}
 

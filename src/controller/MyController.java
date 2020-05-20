8e13cf400b2d8412197b5f0f5ef49b3f68e6ef65
niv kadosh <nivkadosh18192@gmail.com>
2020-05-20 22:01:48 +0300

    added controller

---
+++ src/controller/MyController.java
@@ -0,0 +1,13 @@
+package controller;
+
+import java.util.Observable;
+
+public class MyController implements Controller {
+
+	@Override
+	public void update(Observable o, Object arg) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

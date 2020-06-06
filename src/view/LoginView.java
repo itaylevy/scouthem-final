484104c93e18a43ad899a2299ee900ffe0423b29
Itay Levy <levyetay@gmail.com>
2020-06-06 15:54:26 +0300

    update login file

--- src/view/LoginView.java
+++ src/view/LoginView.java
@@ -1,65 +1,61 @@
 package view;
 
-import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.*;
 
 public class LoginView extends JFrame{
-	private static JLabel myTitle;
-	private static JLabel userTitle;
-	private static JLabel passwordTitle;
-	private static JTextField userName;
-	private static JPasswordField password;
-	private static JButton loginButton;
-	private static JLabel successMessage;
+	private JLabel myTitle;
+	private JLabel userTitle;
+	private JLabel passwordTitle;
+	private JTextField userName;
+	private JPasswordField password;
+	private JButton loginButton;
+	private JLabel successMessage;
 	
 	LoginView(){
-//		JFrame myFrame = new JFrame();
-		JPanel panel = new JPanel();
-		myFrame.setSize(400, 400);
-		myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		myFrame.add(panel);
-		panel.setLayout(null);
+		JPanel myPanel = new JPanel();
+		this.setSize(400, 400);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		myPanel.setLayout(null);
 		myTitle = new JLabel("Scouthem");
 		myTitle.setBounds(150, 2, 100, 100);
-		panel.add(myTitle);
+		myPanel.add(myTitle);
 		userTitle = new JLabel("User:");
 		userTitle.setBounds(65, 60, 100, 100);
-		panel.add(userTitle);
+		myPanel.add(userTitle);
 		passwordTitle = new JLabel("Password:");
 		passwordTitle.setBounds(35, 110, 100, 100);
-		panel.add(passwordTitle);
+		myPanel.add(passwordTitle);
 		userName= new JTextField();
 		password= new JPasswordField();
 		userName.setBounds(100, 100, 200, 25);
 		password.setBounds(100, 150, 200, 25);
-		panel.add(userName);
-		panel.add(password);
+		myPanel.add(userName);
+		myPanel.add(password);
 		loginButton = new JButton("Login");
 		loginButton.setBounds(100, 200, 80, 20);
-		panel.add(loginButton);
-		loginButton.addActionListener(new Login());
+		myPanel.add(loginButton);
 		successMessage = new JLabel("");
 		successMessage.setBounds(200, 200, 80, 100);
-		panel.add(successMessage);
-
-	
-	
-	myFrame.setVisible(true);
+		myPanel.add(successMessage);
+		this.add(myPanel);
+		this.setVisible(true);
+		
 	}
-}
 
-	@Override
-	public void actionPerformed(ActionEvent arg0) {
-		// TODO Auto-generated method stub
-		String user = userName.getText();
-		String passWord = password.getText();
-		if (user.equals("Itay") && passWord.equals("1234")) {
-			successMessage.setText("Welcome " + user);
+	public String getUserName(){return userName.getText();}
+	public char[] getPassword(){return password.getPassword();}
+	public void setLoginMessage(Boolean answer) {
+		if (answer) {
+			successMessage.setText("Welcome");
 		}
 		else {
-			successMessage.setText("User " + user + " doesn't exist!");
+			successMessage.setText("User doesn't exist!");
 		}
+	}
+	void addLoginListner(ActionListener loginButtonListner) {
+		loginButton.addActionListener(loginButtonListner);
+
 	}
 }

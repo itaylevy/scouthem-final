d691a8bf99ed2c1e023f58c4d360e2c022ef827b
niv kadosh <nivkadosh18192@gmail.com>
2020-06-07 17:31:57 +0300

    logincontroller

--- src/controller/LoginController.java
+++ src/controller/LoginController.java
@@ -1,5 +1,34 @@
 package controller;
 
-public class LoginController {
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
+import model.userList;
+import view.LoginView;
+
+public class LoginController<T> {
+	private LoginView theView;
+	private userList<T> theModel;
+	
+    public LoginController(LoginView theView, userList<T> theModel) {
+        this.theView = theView;
+        this.theModel = theModel;
+        this.theView.addLoginListener(new LoginListener());
+
+    }
+    
+	class LoginListener implements ActionListener{
+	
+	    public void actionPerformed(ActionEvent e) {
+	 	   
+	        String userName=null;
+	        char[] password=null;
+	
+	      
+	            userName = theView.getUserName();
+	            password = theView.getPassword();
+	             
+	           theView.setLoginMessage(theModel.login(userName, password));
+		}
+	}
 }

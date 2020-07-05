c7314a54d8b76036c300637c707dc5acb54bdf47
mor cohen <morcohen1515@gmail.com>
2020-07-05 19:13:34 +0300

    add test

--- src/model/testUserList.java
+++ src/model/testUserList.java
@@ -1,44 +1,42 @@
-////package model;
-//
-////import static org.junit.Assert.*;
-//
-////import org.junit.BeforeClass;
-////import org.junit.Test;
-//
-////public class testUserList {
-//	
-//	//private static userList u;
-//	
-//	//@BeforeClass
-//	//public static void setUp() {
-//		u = new userList();
-//	}
-//
-//	@Test
-//	public void AddUserTest() {
-//		user p = new player("mor", "hapoel tel aviv", "center back", 15, 1.83, 76.5, "mor@gmail.com", 123456789, "morcohen", "123456");
-//		user s = new scout("itay", "hapoel tel aviv", 123456786, "itaylevi", "123456");
-//		
-//		u.addUser(p);
-//		u.addUser(s);
-//		
-//		int n = u.getUserList().size();
-//		assertEquals("when adding new two users, number of users should be 2!",2 ,n);
-//	}
-//
-//	//@Test
-//	//public void LoginTest() {
-//	//	fail("Not yet implemented");
-//	//}
-//
-//	//@Test
-//	//public void SingUpPlayerTest() {
-//	//	fail("Not yet implemented");
-//	//}
-//
-//	//@Test
-//	//public void SingUpScoutTest() {
-//	//	fail("Not yet implemented");
-//	//}
-//
-//}
+package model;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class testUserList {
+	
+	private static userList u;
+	
+	@BeforeClass
+	public static void setUp() {
+		u = new userList();
+	}
+
+	@Test
+	public void loginTest() {
+		
+		u.singUpPlayer("mor", "hapoel tel aviv", "center back", 15, 1.83, 76.5, "mor@gmail.com", 123456789, "morcohen", "123456");
+		u.singUpScout("itay", "hapoel tel aviv", 123456786, "itaylevi", "123456");
+		
+		int n = u.getUserList().size();
+		assertEquals("when adding new two users, number of users should be 2!",2 ,n);
+	}
+
+	//@Test
+	//public void LoginTest() {
+	//	fail("Not yet implemented");
+	//}
+
+	//@Test
+	//public void SingUpPlayerTest() {
+	//	fail("Not yet implemented");
+	//}
+
+	//@Test
+	//public void SingUpScoutTest() {
+	//	fail("Not yet implemented");
+	//}
+
+}
